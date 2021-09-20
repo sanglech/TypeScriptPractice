5e6ab7c0d2621f724a90c76be5abd1894f3e730e
Christian <christian@sanglech.com>
2021-09-20 21:59:17 +0900

    class init commit

--- Module_05/dist/app.js
+++ Module_05/dist/app.js
@@ -1,3 +1,24 @@
 "use strict";
 console.log("Hello World");
+var Department = /** @class */ (function () {
+    function Department(n) {
+        this.employees = [];
+        this.name = n;
+    }
+    Department.prototype.describe = function () {
+        console.log("Department: " + this.name);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInfo = function () {
+        console.log("Employees length: " + this.employees.length);
+        console.log("Employees: " + this.employees);
+    };
+    return Department;
+}());
+var department = new Department("Development");
+department.describe();
+department.addEmployee("Christian");
+department.printEmployeeInfo();
 //# sourceMappingURL=app.js.map
--- Module_05/src/app.ts
+++ Module_05/src/app.ts
@@ -1,1 +1,32 @@
 console.log("Hello World")
+
+class Department{
+    private name : string;
+    private employees:string[]=[];
+
+    constructor(n:string){
+        this.name=n;
+
+    }
+
+    describe(){
+        console.log("Department: " + this.name)
+    }
+
+    addEmployee(employee:string){
+        this.employees.push(employee)
+    }
+
+    printEmployeeInfo(){
+        console.log("Employees length: " + this.employees.length);
+        console.log("Employees: "+this.employees)
+    }
+}
+
+const department =new Department("Development")
+
+department.describe();
+
+department.addEmployee("Christian")
+department.printEmployeeInfo();
+
